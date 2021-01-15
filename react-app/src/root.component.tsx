024ade99b11cec1bf7480ae10cfb1c1921951e9c
Manuel Mauky <manuel.mauky@zeiss.com>
2021-01-15 15:30:59 +0100

    Add new routes to react app to test deep linking

--- react-app/src/root.component.tsx
+++ react-app/src/root.component.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { FC } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {Something} from './something';
+import {Other} from './other';
 
-export default function Root(props) {
-    return <h1>React App</h1>;
-}
+export const Root: FC = () => (
+    <Router>
+        <h1>React App</h1>
+        <p>React Routes:</p>
+        <ul>
+            <li><Link to="/react/something">Something</Link></li>
+            <li><Link to="/react/other">Other</Link></li>
+            <li><Link to="/angular">Link to Angular app</Link></li>
+        </ul>
+
+        <Switch>
+            <Route path="/react/something">
+                <Something/>
+            </Route>
+            <Route path="/react/other">
+                <Other/>
+            </Route>
+        </Switch>
+    </Router>
+)
---
+++ react-app/src/something.tsx
@@ -0,0 +1,5 @@
+import React, {FC} from 'react'
+
+export const Something: FC = () => (
+    <p>React Page: Something</p>
+)
